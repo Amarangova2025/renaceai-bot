e7de3d634d3ce393a64535b3f77b577a13851608
Amarangova2025 <amavbootcamps@gmail.com>
2025-06-08 21:04:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = './renaceai-bot-ysvq-fdc8b8d49c9c.json';
 
-const sessionClient = new SessionsClient();
-const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 10000;
